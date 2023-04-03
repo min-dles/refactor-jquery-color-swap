87d4f23f84a93229f2328ff46b616f025f9bea37
mandi andrade <mandi.andrade20@gmail.com>
2023-04-03 13:50:39 -0500

    initial files created and checked for links between css html and js

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('You are ready to code more! 😎');
+}
+
+// Follow the steps below to complete the code challenge:
+
+// 1. Create a `<button>` element on the DOM, you may do this in jQuery, 
+    // or write it directly in the HTML code. Have the button text read "Generate". 
+// 2. Using jQuery, append a `<div>` element when you click the button. 
+    // It should append a new div each time you click the button.
+// 3. Inside the `<div>` element you created in the previous step, 
+    // create a `<p>` element that shows how many times you have clicked the 
+    // "Generate" button from the first step. It should reflect the count for that 
+    // step - the first div you made should be 1, the second 2, etc. 
+    // (You can use the same append from the previous step)
+// 4. Inside the `<div>` element created in step two, append two `<button>` 
+        // elements with text of "Yellow" & "Delete". (You can use the same append from the previous step)
+// 5. In a CSS file, any `<div>` created using the "Generate" button should 
+    // start with red as it's `background-color`.
+// 6. Clicking a "Yellow" `<button>` should change its parent `background-color` from red to yellow.
+// 7. Clicking a "Delete" `<button>` should remove its parent `<div>` and 
+    // that `<div>s` children (including the clicked on "Delete" button). 
